d5d8d84e2f06297bd9b58d61fce91ab7d8c34e01
KathaAmsterdam <34440544+KathaAmsterdam@users.noreply.github.com>
2017-12-17 10:44:10 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -138,37 +138,3 @@ function handleResultsChristmas(response_body) {
 		image.appendTo("#photo-container-christmas")
 	})
 }
-
-      function myMap() {
-      var mapProp= {
-          center:new google.maps.LatLng(52.3730601,4.8920707),
-          zoom:12,
-      };
-      var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-      
-      var myCenter1 = new google.maps.LatLng(52.399017,4.894131);
-      var marker = new google.maps.Marker({position:myCenter1});
-      marker.setMap(map);
-
-      var myCenter2 = new google.maps.LatLng(52.367523,4.888466);
-      var marker = new google.maps.Marker({position:myCenter2});
-      marker.setMap(map);
-
-
-      var myCenter3 = new google.maps.LatLng(52.3680606,4.8900954);
-      var marker = new google.maps.Marker({position:myCenter3});
-      marker.setMap(map);
-
-      var myCenter4 = new google.maps.LatLng(52.3762687,4.8875106);
-      var marker = new google.maps.Marker({position:myCenter4});
-      marker.setMap(map);
-
-      var myCenter5 = new google.maps.LatLng(52.3537736,4.9095949);
-      var marker = new google.maps.Marker({position:myCenter5});
-      marker.setMap(map);
-
-      var myCenter6 = new google.maps.LatLng(52.3587285,4.8820628);
-      var marker = new google.maps.Marker({position:myCenter6});
-      marker.setMap(map);                                                             
-                                                          
-      }
